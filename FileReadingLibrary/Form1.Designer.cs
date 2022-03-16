5bef68b7312513eddd9335194657912568212bbf
El Mahdi Fajoui <el.mahdi.fajoui@outlook.com>
2022-03-16 21:19:30 +0100

    Add project files.

---
+++ FileReadingLibrary/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace FileReadingLibrary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenFile = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.Location = new System.Drawing.Point(24, 12);
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.Size = new System.Drawing.Size(75, 23);
+            this.OpenFile.TabIndex = 0;
+            this.OpenFile.Text = "OpenFile";
+            this.OpenFile.UseVisualStyleBackColor = true;
+            this.OpenFile.Click += new System.EventHandler(this.OpenFileEvent);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(24, 41);
+            this.Output.Name = "Output";
+            this.Output.ReadOnly = true;
+            this.Output.Size = new System.Drawing.Size(752, 388);
+            this.Output.TabIndex = 2;
+            this.Output.Text = "";
+            this.Output.TextChanged += new System.EventHandler(this.Output_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.OpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenFile;
+        private System.Windows.Forms.RichTextBox Output;
+    }
+}
+
---
+++ FileReadingLibrary/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileReadingLibrary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void OpenFileEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Output_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
